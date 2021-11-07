3913591461635c1268852aee8e23fa5a25187c76
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-07 01:29:29 -0300

    feat: add students schema and validation packages

---
+++ src/graphql/students_db/schemas/students.ts
@@ -0,0 +1,22 @@
+import { gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  type Student {
+    name: String!
+    cpf: String!
+    email: String!
+  }
+
+  type Query {
+    listStudents(text: String): [Student]
+    getStudent(cpf: String!): Student
+  }
+
+  type Mutation {
+    addStudent(name: String!, cpf: String!, email: String!): Student!
+    updateStudent(cpf: String!, name: String!, email: String!): Student!
+    deleteStudent(cpf: String!): String!
+  }
+`;
+
+export default typeDefs;
