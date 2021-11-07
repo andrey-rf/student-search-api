3f9b3ad4916224fee1811f08507eca61e83d1efb
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-07 01:42:20 -0300

    feat: add entrypoint code

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
+import { resolvers } from "./graphql/students_db/resolvers";
+import typeDefs from "./graphql/students_db/schemas/students"
+
+dotenv.config();
+
+const PORT = process.env.SERVER_PORT || 4000;
+
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use("/graphql", express.static("public"));
+
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+}
+
+startServer();
