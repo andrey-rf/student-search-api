650f1f4006ff82db9f214aa316263a7bca6769d9
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-07 01:18:14 -0300

    chore: move knexfile to src and use typescript

---
+++ src/knexfile.ts
@@ -0,0 +1,19 @@
+import "ts-node";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/src/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/src/db/seeds',
+    }
+  },
+
+  production: {}
+};
