12adfddae21f130c19f78fe3bad921fa29c0cbd5
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-07 01:28:05 -0300

    feat: add .env.example and db connection file

---
+++ src/db/connect.ts
@@ -0,0 +1,9 @@
+import knex from "knex"
+import knexConfig from "../knexfile";
+
+type Env = "development" | "production"
+
+const environment: Env = process.env.NODE_ENV as Env || "development";
+const config = knexConfig[environment];
+
+export default knex(config);
